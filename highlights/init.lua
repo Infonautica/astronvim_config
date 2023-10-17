ef6b014eb54ebac5da2f9d489fdd13bae4ab719b
Leonid Danilov <leonid989@gmail.com>
2023-10-17 18:31:56 +0300

    Added customizations to transparent theme

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,16 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  
+  -- BG of the line where cursor is
+  CursorLine = { bg = "none" },
+
+  -- File navigation
+  NeoTreeNormal = { bg = "none" },
+  NeoTreeNormalNC = { bg = "none" },
+  NeoTreeTabInactive = { bg = "none" },
+  NeoTreeTabSeperatorActive = { bg = "none" },
+  NeoTreeTabSeperatorInactive = { bg = "none" },
+  NvimTreeTabSeperatorActive = { bg = "none" },
+  NvimTreeTabSeperatorInactive = { bg = "none" },
+  MiniTabLineFill = { bg = "none" },
 }
