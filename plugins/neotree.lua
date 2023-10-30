a9d3f1f4e26839f21791f1db301257315555ae9a
Leonid Danilov <leonid989@gmail.com>
2023-10-30 14:53:37 +0200

    Enabled winbar

--- plugins/neotree.lua
+++ plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts) -- override the options using lazy.nvim
     opts.source_selector = { -- change the header section value
-      winbar = false,
+      winbar = true,
       statusline = false
     }
   end,
